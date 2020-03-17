66f19e8c873816de9bfb519603cfdc6f4f1b8043
Jay <jason_milton@hotmail.com>
2020-03-16 23:59:34 -0400

    added some js to rotate menu'wheel', can use arrow icons

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('hi');
+const circle = document.getElementById('circle');
+const upBtn = document.getElementById('upBtn');
+const downBtn = document.getElementById('downBtn');
+
+let rotateValue = circle.style.transform;
+let rotateSum;
+
+upBtn.onclick = function() {
+  rotateSum = `${rotateValue}rotate(-90deg)`;
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+};
