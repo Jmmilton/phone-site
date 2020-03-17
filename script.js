e25812924cfaa8a755fd07def682a013c8cdb4f9
Jay <jason_milton@hotmail.com>
2020-03-17 18:58:58 -0400

    added hover effects, added arrow key function, but need to fix it

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('hi');
 const circle = document.getElementById('circle');
 const upBtn = document.getElementById('upBtn');
 const downBtn = document.getElementById('downBtn');
@@ -6,14 +5,29 @@ const downBtn = document.getElementById('downBtn');
 let rotateValue = circle.style.transform;
 let rotateSum;
 
-upBtn.onclick = function() {
-  rotateSum = `${rotateValue}rotate(-90deg)`;
-  circle.style.transform = rotateSum;
-  rotateValue = rotateSum;
-};
-
-downBtn.onclick = function() {
+function rotDown() {
   rotateSum = `${rotateValue}rotate(90deg)`;
   circle.style.transform = rotateSum;
   rotateValue = rotateSum;
-};
+}
+
+function rotUp() {
+  rotateSum = `${rotateValue}rotate(-90deg)`;
+  circle.style.transform = rotateSum;
+  rotateValue = rotateSum;
+}
+
+upBtn.addEventListener('click', rotUp);
+downBtn.addEventListener('click', rotDown);
+document.addEventListener('keydown', checkKey);
+
+function checkKey(e) {
+  e = e || window.event;
+
+  if (e.keyCode == '38') {
+    rotUp(); // up arrow
+  } else if (e.keyCode == '40') {
+    rotDown(); // down arrow
+  }
+  e.preventDefault();
+}
